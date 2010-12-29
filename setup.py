1488d8e3f38da7825445333206d6ced75ad55ce0
Éric Lemoine <eric.lemoine@gmail.com>
2010-12-29 22:56:14 +0100

    more stuff in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 from setuptools import setup, find_packages
@@ -14,30 +15,39 @@ requires = [
     'CouchDB==0.8'
     ]
 
-setup(name='OSMQA',
-      version='0.0',
-      description='OSMQA',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="osmqa",
-      entry_points = """\
+setup(
+    name                 = 'OSMQA',
+    version              = '0.1',
+    license              = 'GPLv3',
+    zip_safe             = False,
+    include_package_data = True,
+    keywords             = 'OSM GIS',
+    author               = 'François Van Der Biest',
+    author_email         = 'francois.vanderbiest@gmail.com',
+    url                  = 'http://www.qualitystreetmap.org/',
+    description          = 'OSMQA is an OSM data quality assessment tool',
+    long_description     = README + '\n\n' +  CHANGES,
+    classifiers          = [
+      'Development Status :: 1 - Planning',
+      'Intended Audience :: Information Technology',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'License :: OSI Approved :: Modified BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Framework :: Pylons',
+      'Topic :: Scientific/Engineering :: GIS',
+      'Topic :: Internet :: WWW/HTTP',
+      'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      'Topic :: Internet :: WWW/HTTP :: WSGI',
+      ],
+    packages=find_packages(),
+    install_requires=requires,
+    tests_require=requires,
+    test_suite="osmqa",
+    entry_points = """\
       [paste.app_factory]
       main = osmqa:main
       """,
-      paster_plugins=['pyramid'],
-      )
-
+    paster_plugins=['pyramid'],
+)
